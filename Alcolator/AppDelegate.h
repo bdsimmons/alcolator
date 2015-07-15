dd562607728026360335a966f19d54016d3f04c1
Ben <ben.simmons@trinityis.com>
2015-07-15 00:22:49 -0500

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Benjamin Simmons on 7/15/15.
+//  Copyright (c) 2015 Ben Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
