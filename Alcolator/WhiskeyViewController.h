ad9c8d2c07e4cfebcead75ae7469795c3df75a18
Ben <ben.simmons@trinityis.com>
2015-07-17 12:45:24 -0500

    Implement Calculations

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Benjamin Simmons on 7/17/15.
+//  Copyright (c) 2015 Ben Simmons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
